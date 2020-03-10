3f77955208f33b11f989be6bef74f18e071e1994
cgonzalez@softcaribbean.com <cgonzalez@softcaribbean.com>
2020-03-10 11:51:05 -0500

    Commit 1

---
+++ src/app/pages/graficas-inicial/graficas-inicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas-inicial',
+  templateUrl: './graficas-inicial.component.html',
+  styles: []
+})
+export class GraficasInicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
